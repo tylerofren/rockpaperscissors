317417d82c43bab2a92a4894de352b44794513a4
Tyler Ofreneo <tylerofreneo@gmail.com>
2024-06-29 20:37:28 -0700

    yes

--- main.js
+++ main.js
@@ -15,38 +15,54 @@ function getHumanChoice() {
     return prompt('Enter your move!');
 }
 
-function playGame() {
-    let humanScore = 0, computerScore = 0;
-    let gamesPlayed = 5;
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice[0].toUpperCase() + humanChoice.substring(1).toLowerCase();
-        console.log(humanChoice);
-        console.log(computerChoice);
-        if (humanChoice == computerChoice) {
-            console.log('Same moves. It\'s a tie!');
-        }
-        else if ((humanChoice == 'Rock' && computerChoice == 'Paper') ||
-            (humanChoice == 'Paper' && computerChoice == 'Scissors') ||
-            (humanChoice == 'Scissors' && computerChoice == 'Rock')) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        else {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        console.log(`The score is ${humanScore} (you) ${computerScore} (computer).`)
+let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.substring(1).toLowerCase();
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (humanChoice == computerChoice) {
+        results.textContent = 'Same moves. It\'s a tie!';
     }
-    for (let i = 0; i < gamesPlayed; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log('You win.');
-    }
-    else if (computerScore > humanScore) {
-        console.log('You lose.');
+    else if ((humanChoice == 'Rock' && computerChoice == 'Paper') ||
+        (humanChoice == 'Paper' && computerChoice == 'Scissors') ||
+        (humanChoice == 'Scissors' && computerChoice == 'Rock')) {
+        results.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+        computerScore++;
     }
     else {
-        console.log('Tie.');
+        results.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        humanScore++;
     }
+    results.textContent = `The score is ${humanScore} (you) ${computerScore} (computer).`;
 }
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const results = document.querySelector("#results");
+
+rock.addEventListener('click', () => {
+    playRound('Rock', getComputerChoice());
+    checkWinner()
+})
+paper.addEventListener('click', () => {
+    playRound('Paper', getComputerChoice());
+    checkWinner()
+})
+scissors.addEventListener('click', () => {
+    playRound('Scissors', getComputerChoice());
+    checkWinner()
+})
+
+function checkWinner() {
+    if (humanScore == 5) {
+        results.textContent = 'You win.';
+    }
+    else if (computerScore == 5) {
+        results.textContent = 'You lose.';
+    }    
+}
+
+
